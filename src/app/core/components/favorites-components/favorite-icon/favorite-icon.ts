52a0461d82fd646159334341348fae5ceae9721a
AbdelrahmanKhalid710 <abdelrahmankhalid148@gmail.com>
2025-10-15 17:43:32 +0300

    FavoriteIcon-Component
    Favorite-Icon components added

---
+++ src/app/core/components/favorites-components/favorite-icon/favorite-icon.ts
@@ -0,0 +1,45 @@
+import { Component, Input } from '@angular/core';
+import { Favorites } from '../../../services/favorites/favoritesService';
+
+@Component({
+  selector: 'app-favorite-icon',
+  imports: [],
+  templateUrl: './favorite-icon.html',
+  styleUrl: './favorite-icon.css'
+})
+export class FavoriteIcon {
+  @Input() productId!: string;
+  @Input() favoritesList!: string[];
+
+  isFavorite = false;
+  loading = false;
+
+  constructor(private favoritesService: Favorites) { }
+  ngOnInit() {
+    this.isFavorite = this.favoritesList.includes(this.productId);
+  }
+  toggleFavorite() {
+    this.loading = true;
+    if (this.isFavorite) {
+      this.favoritesService.removeProductFromFavoriteList(this.productId).subscribe({
+        next: () => {
+          this.isFavorite = false;
+          this.loading = false;
+        },
+        error: () => {
+          this.loading = false;
+        }
+      });
+    } else {
+      this.favoritesService.addProductToFavoriteList(this.productId).subscribe({
+        next: () => {
+          this.isFavorite = true;
+          this.loading = false;
+        },
+        error: () => {
+          this.loading = false;
+        }
+      });
+    }
+  }
+}
