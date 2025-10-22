1062cf31495c144a1dc08ee08e96d3bf9bfad924
AbdallahAssem <abdallahassem3@gmail.com>
2025-10-23 00:53:35 +0300

    updateRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,16 @@ import { FavoritesList } from './core/components/favorites-components/favorites-
 import { Home } from './core/components/Home/home/home';
 import { CartComponent } from './core/components/shopping-cart/cart-component/cart-component/cart-component';
 import { UserProfile } from './core/components/Auth/UserProfile/user-profile/user-profile';
+import { ProductsList } from './core/components/Products/products-list/products-list';
+import { ProductDetails } from './core/components/Products/product-details/product-details';
 
 export const routes: Routes = [
   { path: 'login', component: UserLogin },
   { path: 'register', component: UserRegister },
   { path: 'favorites', component: FavoritesList, canActivate: [authGuard] },
   { path: 'cart', component: CartComponent },
+  { path: 'products', component: ProductsList},
+  { path: 'products/:id', component: ProductDetails},
   { path: 'home', component: Home },
   {
     path: 'profile', component: UserProfile, canActivate: [authGuard] },
