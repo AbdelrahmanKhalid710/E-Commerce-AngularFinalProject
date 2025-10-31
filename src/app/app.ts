e008e1c32439fb9a9ceab1bf3d8b122d3505f0ab
AbdallahAssem <abdallahassem3@gmail.com>
2025-10-31 13:39:38 +0200

    UpdateNavbar&Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,51 +1,4 @@
-// import { Component, computed, inject } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { Router, RouterLink, RouterOutlet,NavigationEnd  } from '@angular/router';
-// import { RouterModule } from '@angular/router';
-// import { Login } from './core/services/Auth/login';
-// import { CartBadge } from './core/components/shopping-cart/cart-badge/cart-badge';
 
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [CommonModule, RouterLink, RouterOutlet, CartBadge,RouterModule],
-//   templateUrl: './app.html',
-//   styleUrls: ['./app.css']
-// })
-// export class App {
-//   private loginService = inject(Login);
-//   private router = inject(Router);
-//   currentRoute: string = '';
-//   constructor() {
-//     // Listen to route changes without RxJS operators
-//     this.router.events.subscribe((event) => {
-//       if (event instanceof NavigationEnd) {
-//         this.currentRoute = event.url;
-//       }
-//     });}
-  
-//     isAdminRoute(): boolean {
-//     return this.currentRoute.includes('/admin');
-//   }
-
-//   user = this.loginService.user;
-//   token = this.loginService.token;
-
-//   isLoggedIn = computed(() => !!this.user() && !!this.token());
-
-//   onLoginClick(): void {
-//     if (this.isLoggedIn()) {
-//       this.router.navigate(['/profile']);
-//     } else {
-//       this.router.navigate(['/login']);
-//     }
-//   }
-
-//   logout(): void {
-//     this.loginService.logout();
-//     this.router.navigate(['/login']);
-//   }
-// }
 import { Component, computed, inject, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterOutlet, NavigationEnd } from '@angular/router';
@@ -53,12 +6,20 @@ import { RouterModule } from '@angular/router';
 import { Login } from './core/services/Auth/login';
 import { CartBadge } from './core/components/shopping-cart/cart-badge/cart-badge';
 import { FavoriteIcon } from "./core/components/favorites-components/favorite-icon/favorite-icon";
+import { Navbar } from './core/components/layout/navbar/navbar';
+import { Footer } from './core/components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, CartBadge, RouterModule, FavoriteIcon],
-  templateUrl: './app.html',
+  imports: [CommonModule, RouterLink, RouterOutlet, CartBadge, RouterModule, FavoriteIcon, Navbar, Footer],
+  template: `
+    <app-navbar></app-navbar>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+    <app-footer></app-footer>
+  `,
   styleUrls: ['./app.css']
 })
 export class App {
